052ff44aa30f048251d20fcbf1c1da5d144cd7ac
cdROma <truemrv@gmail.com>
2020-05-21 22:42:09 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,24 +1,7 @@
 import React, {useState} from 'react';
-import {
-  SafeAreaView,
-  Button,
-  Linking,
-  View,
-  ActivityIndicator,
-} from 'react-native';
+import {SafeAreaView, Button} from 'react-native';
 import {WebView} from 'react-native-webview';
 import QR from './QR';
-
-let jsCode =
-  '!function(){var e=function(e,n,t){if(n=n.replace(/^on/g,""),"addEventListener"in window)e.addEventListener(n,t,!1);else if("attachEvent"in window)e.attachEvent("on"+n,t);else{var o=e["on"+n];e["on"+n]=o?function(e){o(e),t(e)}:t}return e},n=document.querySelectorAll("a[href]");if(n)for(var t in n)n.hasOwnProperty(t)&&e(n[t],"onclick",function(e){new RegExp("^https?://"+location.host,"gi").test(this.href)||(e.preventDefault(),window.postMessage(JSON.stringify({external_url_open:this.href})))})}();';
-
-const renderLoadingView = () => {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <ActivityIndicator size="large" />
-    </View>
-  );
-};
 
 export default function App() {
   const [qrIsOpewn, setQrIsOpen] = useState(false);
@@ -32,7 +15,7 @@ export default function App() {
     closeHandler();
   };
   if (urlToWebView) {
-    return <WebView source={{uri: urlToWebView}} style={{marginTop: 20}} />;
+    return <WebView source={{uri: urlToWebView}} />;
   }
   return (
     <SafeAreaView
